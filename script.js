33825072c1774e5da74a246ae1f3fd008514781f
Andrij <62100293+pgmtags@users.noreply.github.com>
2023-08-15 08:56:24 +0300

    Fixes

--- script.js
+++ script.js
@@ -1,57 +1,44 @@
-window.onload = function() {
-  document.querySelector(".header").classList.add("visible");
-};
-
 const tours = [
     {
       image: 'photos/bali.jpg',
       title: 'Mystical Bali Adventure',
       description: 'Explore the enchanting island of Bali, with its lush jungles, vibrant culture...',
-      buttonText: 'Buy Tour'
     },
     {
       image: 'photos/tokyo.jpg',
       title: 'Futuristic Tokyo Odyssey',
       description: 'Discover the captivating blend of tradition and innovation in Tokyo...',
-      buttonText: 'Buy Tour'
     },
     {
       image: 'photos/African_safari.jpg',
       title: 'Majestic African Safari',
       description: 'Embark on a thrilling journey through the African wilderness...',
-      buttonText: 'Buy Tour'
     },
     {
       image: 'photos/Rio_Carnival.jpg',
       title: 'Vibrant Rio de Janeiro Carnival',
       description: "Experience the energy and excitement of Rio de Janeiro's world-famous Carnival...",
-      buttonText: 'Buy Tour'
     },
     {
       image: 'photos/Alaska.jpg',
       title: 'Alaskan Wilderness Expedition',
       description: 'Immerse yourself in the rugged beauty of Alaska, a land of towering mountains...',
-      buttonText: 'Buy Tour'
     },
     {
       image: 'photos/Egypt_pyramids.jpg',
       title: 'Enigmatic Egyptian Odyssey',
       description: 'Uncover the mysteries of ancient Egypt, from the iconic pyramids and Sphinx...',
-      buttonText: 'Buy Tour'
     },
     {
       image: 'photos/Machu_Picchu.jpg',
       title: 'Mystical Machu Picchu Trek',
       description: "Venture deep into the heart of the Andes, where you'll uncover the ancient secrets of Machu Picchu...",
-      buttonText: 'Buy Tour'
     },
     {
       image: 'photos/New_Zealand.jpg',
       title: 'Scenic New Zealand Adventure',
       description: 'Traverse the stunning landscapes of New Zealand, from the lush rainforests and pristine beaches...',
-      buttonText: 'Buy Tour'
     }];
-
 
 const toursContainer = document.querySelector('.tour-cards');
 const createTourCard = (tour) => {
@@ -79,7 +66,7 @@ const createTourCard = (tour) => {
 
   const tourCardButtonBuy = document.createElement('button');
   tourCardButtonBuy.classList.add('tour-card__button', 'tour-card__button-buy');
-  tourCardButtonBuy.textContent = tour.buttonText;
+  tourCardButtonBuy.textContent = 'Buy Tour';
   tourCardButtons.appendChild(tourCardButtonBuy);
 
   const tourCardButtonInfo = document.createElement('button');
@@ -92,22 +79,7 @@ const createTourCard = (tour) => {
   return tourCard;
 };
 
-
 tours.forEach((tour) => {
   const tourCard = createTourCard(tour);
   toursContainer.appendChild(tourCard);
 });
-
-var prevScrollpos = window.pageYOffset;
-
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos || currentScrollPos === 0) {
-    // При прокрутке страницы вверх или когда страница находится в самом верху
-    document.querySelector(".header").classList.add("visible");
-  } else {
-    // При прокрутке страницы вниз
-    document.querySelector(".header").classList.remove("visible");
-  }
-  prevScrollpos = currentScrollPos;
-};
